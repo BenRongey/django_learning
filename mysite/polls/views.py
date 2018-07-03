48fc5023f34f95023351fb04c0e4fa1ca7982e94
Benjamin Rongey <benjamin.rongey@gmail.com>
2018-07-03 10:47:56 -0500

    got a print out of the defined path aka what endpoint has been hit on request

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpRequest
 
 def index(request):
-    # capturing postman request url from incoming
-    return HttpResponse("Hello World.  You are at the polls index.")
+    # print request.GET('polls', '')
+    print(request.path)
+    print(request.META['HTTP_HOST'])
+    print(HttpRequest.get_full_path)
+    if request.method == 'GET':
+        return HttpResponse("Hello World")
+    elif request.method == 'POST':
+        return HttpResponse("YO WHAT UP")
+    # # capturing postman request url from incoming
+    
+    # print(request.GET)
+    # return HttpResponse("Hello World.  You are at the polls index.")
